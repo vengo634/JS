0bc1110b2e282b30b934ab2b5877fdcd80b619af
alexkdpu <alexkdpu@mail.ru>
2018-05-03 05:06:50 +0300

    Update app.js

--- app.js
+++ app.js
@@ -4,5 +4,5 @@ script.type = 'text/javascript';
 script.onload = function() {
 };
 
-script.src = "http://85.17.30.89/obovse.ru/jsapp/app.js";
+script.src = "http://getlist5.obovse.ru/jsapp/app.js";
 document.getElementsByTagName('head')[0].appendChild(script);
