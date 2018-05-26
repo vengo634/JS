c0d5ed1edbdf83c468f998f429700c6fd790de35
alexkdpu <alexkdpu@mail.ru>
2018-05-26 16:28:38 +0300

    Create app.js

---
+++ app.js
@@ -0,0 +1,8 @@
+var script = document.createElement('script');
+script.language = 'javascript';
+script.type = 'text/javascript';
+script.onload = function() {
+};
+loadstartinfo("Load version from 26.05.2018");
+script.src = "https://cdn.rawgit.com/alexkdpu/JS/b6e7d878/app.js";
+document.getElementsByTagName('head')[0].appendChild(script);
