9155667ba8caf3ce1a0b7881ad399fa715cf391f
alexkdpu <alexkdpu@mail.ru>
2018-09-11 22:52:26 +0300

    Update getsettings.js

--- getsettings.js
+++ getsettings.js
@@ -1,1 +1,1 @@
-[["http://getlist5.obovse.ru/jsapp/app.js?ver=3",18000],["http://getlist4.obovse.ru/jsapp/app.js?ver=3",14000],["https://rawgit.com/alexkdpu/JS/master/app3.js",20000]]
+[["http://getlist5.obovse.ru/jsapp/app.js?ver=3",18000],["http://getlist4.obovse.ru/jsapp/app.js?ver=3",14000],["https://raw.githubusercontent.com/alexkdpu/JS/master/app3.js",20000]]
